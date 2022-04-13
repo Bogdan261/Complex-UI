bea79d0e716974cf77eebbdbc440b3b096e0487e
Bogdan261 <0bogdan261@gmail.com>
2022-04-13 18:25:55 +0300

    Work in progress on implementing a clean transition logic to Images Screen

---
+++ Assets/Scripts/Managers/ImagesScreenManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts.Managers
+{
+    public class ImagesScreenManager : MonoBehaviour
+    {
+        public static ImagesScreenManager Instance;
+
+        [HideInInspector]
+        public ImagesListSO ImagesList;
+
+        private void Awake()
+        {
+            if (Instance != null && Instance != this)
+            {
+                Destroy(gameObject);
+            }
+            else if (Instance == null)
+            {
+                Instance = this;
+            }
+        }
+    }
+}
